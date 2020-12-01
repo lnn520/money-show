5469bff76bd615280935eb3b05c1886c4fa4185b
lnn520 <335341543@qq.com>
2020-12-01 21:08:12 +0800

    deploy

---
+++ precache-manifest.4950f4cf061deed1f853f0616e90ac45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6f229f9370a6e918b2",
+    "url": "/money-show/css/app.de5e7865.css"
+  },
+  {
+    "revision": "4a4e71085a2346ddfaf0e6e893623a65",
+    "url": "/money-show/index.html"
+  },
+  {
+    "revision": "1e6f229f9370a6e918b2",
+    "url": "/money-show/js/app.d2b6a900.js"
+  },
+  {
+    "revision": "dd1d470a047925b6750e",
+    "url": "/money-show/js/chunk-vendors.8307601d.js"
+  },
+  {
+    "revision": "7fb09d9122939182bff65f473e335301",
+    "url": "/money-show/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-show/robots.txt"
+  }
+]);
